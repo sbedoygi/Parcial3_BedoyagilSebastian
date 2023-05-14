e0f45cf125c35452d1f5d6a338b7da593d56e982
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-05-14 13:19:36 -0500

    installs package was built for IdentityDbContext<User>

--- Sebas_lavadero/Sebas_lavadero/DAL/DataBaseContext.cs
+++ Sebas_lavadero/Sebas_lavadero/DAL/DataBaseContext.cs
@@ -3,10 +3,28 @@ using Sebas_lavadero.DAL.Entities;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 
-namespace Sebas_lavadero.DAL
+namespace Sebas_lavadero.DAL // se crea la herencia y se estala  los paqutes de framework sacado del video de  clase  11 de abril
 {
-    public class DataBaseContext : DbContext // se crea la herencia y se estala  los paqutes de framework sacado del video de  clase  11 de abril
+    public class DataBaseContext : DbContext
     {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehicleDetail> VehicleDetails { get; set; }
+
+
+      
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Service>().HasIndex(s => s.Name).IsUnique();
+            modelBuilder.Entity<Vehicle>().HasIndex(v => v.Plate).IsUnique();
+            modelBuilder.Entity<VehicleDetail>().HasIndex(vd => vd.Id).IsUnique();
+        }
     }
 
 }
