55593b54e9c998832483610a1c1bbf3be7dfab32
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-05-14 14:26:55 -0500

    create new ASP.NET MVC controllers. dd, Controller  entities

---
+++ Sebas_lavadero/Sebas_lavadero/Controllers/VehicleDetailsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sebas_lavadero.DAL;
+using Sebas_lavadero.DAL.Entities;
+
+namespace Sebas_lavadero.Controllers
+{
+    public class VehicleDetailsController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public VehicleDetailsController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VehicleDetails
+        public async Task<IActionResult> Index()
+        {
+              return _context.VehicleDetails != null ? 
+                          View(await _context.VehicleDetails.ToListAsync()) :
+                          Problem("Entity set 'DataBaseContext.VehicleDetails'  is null.");
+        }
+
+        // GET: VehicleDetails/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.VehicleDetails == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleDetail = await _context.VehicleDetails
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleDetail);
+        }
+
+        // GET: VehicleDetails/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DeliveryDate,Id,CreateDate,ModifiedDate")] VehicleDetail vehicleDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                vehicleDetail.Id = Guid.NewGuid();
+                _context.Add(vehicleDetail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleDetail);
+        }
+
+        // GET: VehicleDetails/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.VehicleDetails == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleDetail = await _context.VehicleDetails.FindAsync(id);
+            if (vehicleDetail == null)
+            {
+                return NotFound();
+            }
+            return View(vehicleDetail);
+        }
+
+        // POST: VehicleDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("DeliveryDate,Id,CreateDate,ModifiedDate")] VehicleDetail vehicleDetail)
+        {
+            if (id != vehicleDetail.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicleDetail);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleDetailExists(vehicleDetail.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleDetail);
+        }
+
+        // GET: VehicleDetails/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.VehicleDetails == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleDetail = await _context.VehicleDetails
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleDetail);
+        }
+
+        // POST: VehicleDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.VehicleDetails == null)
+            {
+                return Problem("Entity set 'DataBaseContext.VehicleDetails'  is null.");
+            }
+            var vehicleDetail = await _context.VehicleDetails.FindAsync(id);
+            if (vehicleDetail != null)
+            {
+                _context.VehicleDetails.Remove(vehicleDetail);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleDetailExists(Guid id)
+        {
+          return (_context.VehicleDetails?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Sebas_lavadero/Sebas_lavadero/Controllers/VehiclesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sebas_lavadero.DAL;
+using Sebas_lavadero.DAL.Entities;
+
+namespace Sebas_lavadero.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public VehiclesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vehicles
+        public async Task<IActionResult> Index()
+        {
+              return _context.Vehicles != null ? 
+                          View(await _context.Vehicles.ToListAsync()) :
+                          Problem("Entity set 'DataBaseContext.Vehicles'  is null.");
+        }
+
+        // GET: Vehicles/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Vehicles == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.Vehicles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vehicles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Owner,Plate,Id,CreateDate,ModifiedDate")] Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                vehicle.Id = Guid.NewGuid();
+                _context.Add(vehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Vehicles == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.Vehicles.FindAsync(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Owner,Plate,Id,CreateDate,ModifiedDate")] Vehicle vehicle)
+        {
+            if (id != vehicle.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleExists(vehicle.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Vehicles == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.Vehicles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Vehicles == null)
+            {
+                return Problem("Entity set 'DataBaseContext.Vehicles'  is null.");
+            }
+            var vehicle = await _context.Vehicles.FindAsync(id);
+            if (vehicle != null)
+            {
+                _context.Vehicles.Remove(vehicle);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleExists(Guid id)
+        {
+          return (_context.Vehicles?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
